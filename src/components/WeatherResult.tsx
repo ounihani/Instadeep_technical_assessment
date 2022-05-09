e5862e4dfef8a5f0c9fd559713f0a27c33ae8b7c
Heni <ouni.hani.fcb@gmail.com>
2022-05-09 16:01:03 +0100

    showing weather cards

--- src/components/WeatherResult.tsx
+++ src/components/WeatherResult.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import WeatherCard from './WeatherCard';
+import { useSelector } from 'react-redux';
+import { RootState } from "../store/store";
+
+
 
 
 export default function WeatherResult() {
+    const { WeatherList, loading } = useSelector((state: RootState) => state.weather)
+
     return (
         <Grid container
             justifyContent="center"
@@ -11,39 +17,20 @@ export default function WeatherResult() {
             spacing={2}
 
         >
+
             <Grid item >
                 back
             </Grid>
-            <Grid item
+
+            {WeatherList.map((daysWeather) => (
+                <Grid item
                 justifyContent="center"
                 alignItems="center"
             >
                 <WeatherCard />
             </Grid>
-            <Grid item
-                justifyContent="center"
-                alignItems="center"
-            >
-                <WeatherCard />
-            </Grid>
-            <Grid item
-                justifyContent="center"
-                alignItems="center"
-            >
-                <WeatherCard />
-            </Grid>
-            <Grid item
-                justifyContent="center"
-                alignItems="center"
-            >
-                <WeatherCard />
-            </Grid>
-            <Grid item
-                justifyContent="center"
-                alignItems="center"
-            >
-                <WeatherCard />
-            </Grid>
+            ))}
+
             <Grid item >
                 next
             </Grid>
